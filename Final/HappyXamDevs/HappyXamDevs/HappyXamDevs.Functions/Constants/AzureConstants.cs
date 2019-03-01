e74cbafb8e3e86f960e3e432d2f6de340eece4a5
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-02-28 17:26:18 -0800

    Update AzureConstants.cs

--- Final/HappyXamDevs/HappyXamDevs/HappyXamDevs.Functions/Constants/AzureConstants.cs
+++ Final/HappyXamDevs/HappyXamDevs/HappyXamDevs.Functions/Constants/AzureConstants.cs
@@ -3,6 +3,8 @@
     public static class AzureConstants
     {
 #error Missing Cosmos Db Connection String
-        public const string CosmosDbConnectionString = "[Your Cosmos Db Connection String]";
+
+#error Missing Blob Storage Connection String
+        public const string BlobStorageConnectionString = "[Your Blob Storage Connection String";
     }
 }
