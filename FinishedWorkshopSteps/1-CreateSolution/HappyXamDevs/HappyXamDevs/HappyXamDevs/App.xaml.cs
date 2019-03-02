6744001ac87b328a38ca5a2e513eb50edda010f7
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-03-02 11:52:59 -0800

    Removed Unused Code

--- FinishedWorkshopSteps/1-CreateSolution/HappyXamDevs/HappyXamDevs/HappyXamDevs/App.xaml.cs
+++ FinishedWorkshopSteps/1-CreateSolution/HappyXamDevs/HappyXamDevs/HappyXamDevs/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace HappyXamDevs
 {
@@ -11,21 +9,6 @@ namespace HappyXamDevs
             InitializeComponent();
 
             MainPage = new MainPage();
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
         }
     }
 }
