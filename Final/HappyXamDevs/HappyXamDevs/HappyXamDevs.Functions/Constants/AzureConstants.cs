d44ce78dd9a62079b68fb90cb14a901d20d7ddce
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-03-02 12:46:10 -0800

    Added CosmosDbConnectionString & ProcessBlobQueueName

--- Final/HappyXamDevs/HappyXamDevs/HappyXamDevs.Functions/Constants/AzureConstants.cs
+++ Final/HappyXamDevs/HappyXamDevs/HappyXamDevs.Functions/Constants/AzureConstants.cs
@@ -1,10 +1,13 @@
-﻿namespace HappyXamDevs.Functions
+namespace HappyXamDevs.Functions
 {
     public static class AzureConstants
     {
 #error Missing Cosmos Db Connection String
+        public const string CosmosDbConnectionString = "[Your Cosmos Db Connection String]"
 
 #error Missing Blob Storage Connection String
-        public const string BlobStorageConnectionString = "[Your Blob Storage Connection String";
+        public const string BlobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=happyxamdevsstoragebrm;AccountKey=WGQdWxyEONgfCsNTaFy/1OHHZZy+UaKgSof2+TJUrsRCPZNu66Yk6u5A62fHPgSrZhrhQ2OEu17KGAbWKjqqOw==;EndpointSuffix=core.windows.net";
+
+        public const string ProcessBlobQueueName = "processblobqueue";
     }
 }
