b471833e35e2f658784670bcd10696c3c7d15e01
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-03-02 20:21:05 -0800

    Removed [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
    Avoids build error from duplicate assembly tag when building from the template

--- Final/HappyXamDevs/HappyXamDevs/HappyXamDevs/App.xaml.cs
+++ Final/HappyXamDevs/HappyXamDevs/HappyXamDevs/App.xaml.cs
@@ -1,9 +1,6 @@
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
-using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 namespace HappyXamDevs
 {
@@ -14,13 +11,14 @@ namespace HappyXamDevs
             InitializeComponent();
 
             var navigationPage = new Xamarin.Forms.NavigationPage(new MainPage());
-            MainPage = navigationPage;
 
             navigationPage.BarBackgroundColor = (Color)Resources["CoolPurple"];
             navigationPage.BarTextColor = Color.White;
 
             navigationPage.On<iOS>().SetPrefersLargeTitles(true);
             navigationPage.On<iOS>().SetUseSafeArea(true);
+
+            MainPage = navigationPage;
         }
     }
 }
