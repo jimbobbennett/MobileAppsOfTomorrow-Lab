3c02a90b6c297045051e25ed683b54343429d6ee
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-03-03 10:10:13 -0800

    Updated Workshop 1

--- FinishedWorkshopSteps/1-CreateSolution/HappyXamDevs/HappyXamDevs/HappyXamDevs/App.xaml.cs
+++ FinishedWorkshopSteps/1-CreateSolution/HappyXamDevs/HappyXamDevs/HappyXamDevs/App.xaml.cs
@@ -1,4 +1,6 @@
+﻿using System;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace HappyXamDevs
 {
@@ -9,6 +11,21 @@ namespace HappyXamDevs
             InitializeComponent();
 
             MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
         }
     }
 }
--- FinishedWorkshopSteps/1-CreateSolution/HappyXamDevs/HappyXamDevs/HappyXamDevs/MainPage.xaml.cs
+++ FinishedWorkshopSteps/1-CreateSolution/HappyXamDevs/HappyXamDevs/HappyXamDevs/MainPage.xaml.cs
@@ -1,3 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace HappyXamDevs
